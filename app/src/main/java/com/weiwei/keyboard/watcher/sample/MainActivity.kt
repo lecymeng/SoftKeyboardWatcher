a5aa04b08861791c813a94cf4a0d8fa95815bb6d
Weiwei <1570682285@qq.com>
2022-10-03 17:14:45 +0800

    test dialog

--- app/src/main/java/com/weiwei/keyboard/watcher/sample/MainActivity.kt
+++ app/src/main/java/com/weiwei/keyboard/watcher/sample/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.updateLayoutParams
+import com.weiwei.keyboard.watcher.SoftKeyboardWatcher
 import com.weiwei.keyboard.watcher.sample.databinding.ActivityMainBinding
 import com.weiwei.keyboard.watcher.sample.test.ImeTestActivity
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,8 +56,53 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             R.id.dialog_test -> {
-                val sendFragment = SendFragment()
-                sendFragment.show(supportFragmentManager, "send")
+                // val sendFragment = SendFragment()
+                // sendFragment.show(supportFragmentManager, "send")
+
+                val alertDialog = AlertDialog.Builder(this)
+                    .setView(R.layout.fragment_second)
+                    .create()
+                alertDialog.show()
+                alertDialog.setOnShowListener {
+                    val buttonSend: View = alertDialog.findViewById(R.id.buttonSend)!!
+                    val textInputLayout: View = alertDialog.findViewById(R.id.textInputLayout)!!
+                    alertDialog.window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+
+                    // fixme dialog test
+                    val softInputAnimDelegate = SoftInputAnimDelegate()
+                    val keyboardWatcher = SoftKeyboardWatcher(alertDialog.window!!)
+                    keyboardWatcher.startWatch(this, this) { imeHeight, navigationBarsHeight, animated ->
+                        val translation = -max(imeHeight - navigationBarsHeight, 0).toFloat()
+                        if (animated) {
+                            // Android 11+, Window insets animation callback
+                            // set views padding/margin/translationY
+                            buttonSend.translationY = translation
+                            textInputLayout.translationY = translation
+                        } else {
+                            // set views padding/margin/translationY with animation or without animation
+
+                            if (imeHeight > 0) {
+                                buttonSend.alpha = 0f
+                                textInputLayout.alpha = 0f
+                                buttonSend.translationY = translation
+                                textInputLayout.translationY = translation
+                                val areaHeight = textInputLayout.top - buttonSend.bottom
+                                softInputAnimDelegate.showSoftInput(translation - areaHeight, translation) { anim ->
+                                    buttonSend.alpha = anim.animatedFraction
+                                    textInputLayout.alpha = anim.animatedFraction
+                                    // val animatedValue = anim.animatedValue as Float
+                                    // buttonSend.translationY = animatedValue
+                                    // textInputLayout.translationY = animatedValue
+                                }
+                            } else {
+                                softInputAnimDelegate.hideSoftInput(buttonSend.translationY) { animatedValue ->
+                                    buttonSend.translationY = animatedValue
+                                    textInputLayout.translationY = animatedValue
+                                }
+                            }
+                        }
+                    }
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
